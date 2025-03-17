145e0b19f7011b2fea7aaed84d23f220848d5406
bharath.rajashekar3@gmail.com <bharath.rajashekar3@gmail.com>
2025-03-17 23:05:10 +0530

    removed non-working components

--- app/dashboard/_components/Sidebar.jsx
+++ app/dashboard/_components/Sidebar.jsx
@@ -24,18 +24,18 @@ function Sidebar() {
       icon: <HiOutlineCollection />,
       path: "/dashboard/explore",
     },
-    {
-      id: 3,
-      name: "Upgrade",
-      icon: <HiOutlineShieldCheck />,
-      path: "/dashboard/upgrade",
-    },
-    {
-      id: 4,
-      name: "Logout",
-      icon: <HiOutlineExclamationCircle />,
-      path: "/dashboard/logout",
-    },
+    // {
+    //   id: 3,
+    //   name: "Upgrade",
+    //   icon: <HiOutlineShieldCheck />,
+    //   path: "/dashboard/upgrade",
+    // },
+    // {
+    //   id: 4,
+    //   name: "Logout",
+    //   icon: <HiOutlineExclamationCircle />,
+    //   path: "/dashboard/logout",
+    // },
   ];
   const path = usePathname();
   return (
@@ -45,25 +45,24 @@ function Sidebar() {
 
       <ul>
         {Menu.map((item, index) => (
-          <Link href={item.path} key={item.id}> 
-          <div
-            className={`flex items-center gap-2 text-gray-600 
+          <Link href={item.path} key={item.id}>
+            <div
+              className={`flex items-center gap-2 text-gray-600 
             p-3 cursor-pointer hover:bg-gray-100
              hover:text-black rounded-lg mb-3
              ${item.path == path && "bg-gray-100 text-black"} `}
-          >
-            <div className="text-2xl">{item.icon}</div>
-            <h2>{item.name}</h2>
-          </div>
+            >
+              <div className="text-2xl">{item.icon}</div>
+              <h2>{item.name}</h2>
+            </div>
           </Link>
         ))}
       </ul>
 
-      <div className='absolute bottom-10 w-[80%]'>
-      {/* <Progress value={33} />
+      <div className="absolute bottom-10 w-[80%]">
+        {/* <Progress value={33} />
       <h2 className='text-sm my-2'>3 out of 5 course created</h2>
       <h2 className="text-xs text-gray-500">Upgrade your plan</h2> */}
-
       </div>
     </div>
   );
