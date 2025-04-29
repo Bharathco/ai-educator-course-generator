05bf1c18edde11f72b7698668cd4b417cf2d9bc4
bharath.rajashekar3@gmail.com <bharath.rajashekar3@gmail.com>
2025-04-29 13:33:27 +0530

    updated service.jsx

--- configs/service.jsx
+++ configs/service.jsx
@@ -1,27 +1,21 @@
 const { default: axios } = require("axios");
 
-const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3";
+const YOUTUBE_BASE_URL='https://www.googleapis.com/youtube/v3';
 
-const getVideos = async (query) => {
-  const params = {
-    part: "snippet",
-    q: query,
-    maxResults: 1,
-    type: "video",
-    order: "relevance", // Get relevant results
-    videoDuration: "any", // Include all video lengths
-    key: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
-  };
+const getVideos=async(query)=>{
+    const params={
+        part:'snippet',
+        q:query,
+        maxResults:1,
+        type:'video',
+        key:process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+    }
 
-  try {
-    const resp = await axios.get(YOUTUBE_BASE_URL + "/search", { params });
+    const resp=await axios.get(YOUTUBE_BASE_URL+'/search',{params});
+
     return resp.data.items;
-  } catch (error) {
-    console.error("Error fetching videos: ", error);
-    return []; // Return empty array on error
-  }
-};
+}
 
-export default {
-  getVideos,
-};
+export default{
+    getVideos
+}
